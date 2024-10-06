d006750955d7fbca4c7813a1e5a2965616a86d71
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2024-10-06 00:40:14 -0700

    finished pickplant page

--- PlantPals/app/index.tsx
+++ PlantPals/app/index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
         </View>
         <Pressable style={styles.buttonStyle} onPress={() => {
           // router.push('/camera')
-          router.push("/api?token=gWkJd5BMXBtyDMc")
+          router.push("/pickplant?token=DqjeBDTc44Y1Xmp&name=bob")
         }}>
           <Text style={styles.buttonText}>Add Your Plant</Text>
         </Pressable>
