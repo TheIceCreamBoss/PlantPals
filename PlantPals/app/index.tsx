2076cb83eb97997788acc69851c8714755e96e1e
Erik Lin <erik.s.lin@getgosoft.com>
2024-10-06 01:04:52 -0700

    Updated index routing

--- PlantPals/app/index.tsx
+++ PlantPals/app/index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
         </View>
         <Pressable style={styles.buttonStyle} onPress={() => {
           // router.push('/camera')
-          router.push("/pickplant?token=DqjeBDTc44Y1Xmp&name=bob")
+          router.push("/api?token=DqjeBDTc44Y1Xmp")
         }}>
           <Text style={styles.buttonText}>Add Your Plant</Text>
         </Pressable>
